5b86ddf1fda2f6fa2a7ecb75ab877327f164cc55
Jordan <jordanliebman329@gmail.com>
2018-11-26 13:57:35 -0600

    list of places displayed in listbox!

--- TestWPF/MainWindow.xaml.cs
+++ TestWPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace TestWPF
 {
@@ -27,10 +31,40 @@ namespace TestWPF
         
         private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
+
+            JObject x = new JObject(Get("https://xqgy0d8a3l.execute-api.us-east-1.amazonaws.com/Prod/places/" + ZipCodeTextBox.Text));
+            IList<JToken> results = x["results"].Children().ToList();
+
+            IList<Place> searchResults = new List<Place>();
+            foreach (JToken result in results)
+            {
+                // JToken.ToObject is a helper method that uses JsonSerializer internally
+                Place searchResult = result.ToObject<Place>();
+                searchResults.Add(searchResult);
+            }
+
+            Console.WriteLine(searchResults);
+            foreach(Place searchResult in searchResults)
+            {
+                ListBoxItem placeListBoxItem = new ListBoxItem();
+                placeListBoxItem.Content = searchResult.Name;
+                PlacesListBox.Items.Add(placeListBoxItem);
+            }
             
-            ListBoxItem placeListBoxItem = new ListBoxItem();
-            placeListBoxItem.Content = ZipCodeTextBox.Text;
-            PlacesListBox.Items.Add(placeListBoxItem);
+        }
+        public JObject Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                JObject o = JObject.Parse(reader.ReadToEnd());
+                return o;
+
+            }
         }
     }
 }
---
+++ TestWPF/Place.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWPF
+{
+    class Place
+    {
+        public string Name { get; set; }
+        public string Place_Id { get; set; }
+    }
+}
