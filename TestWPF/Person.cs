9c13969d56d05b9f3e9da364025541152a32a47b
Jordan <jordanliebman329@gmail.com>
2018-11-26 10:47:42 -0600

    Add project files.

---
+++ TestWPF/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWPF
+{
+    abstract class Person
+    {
+        private string firstName;
+        private string lastName;
+        /// <summary>
+        /// 0 for male
+        /// 1 for female
+        /// 2 for other
+        /// </summary>
+        private int gender;
+        private int age;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public int Gender
+        {
+            get
+            {
+                return gender;
+            }
+            set
+            {
+                gender = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+    }
+}
